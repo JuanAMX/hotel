9e1a2134f50db0bacebbca64797e2ae0423c06c8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-06 22:59:39 +0000

    Deploy JuanAMX/hotel to github.com/JuanAMX/hotel.git:master

---
+++ precache-manifest.da971b196d5fb4a6f6942e32cc775adc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a10154982f23baa5f5c62306b87460",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7749ac4c78c0272019f4",
+    "url": "/static/css/main.770e2e33.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "7749ac4c78c0272019f4",
+    "url": "/static/js/main.5257f220.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
